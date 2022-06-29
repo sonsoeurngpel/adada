e13dfec785e395d7694795bf1c79138727fc0ffb
Sonsoeurng Pel <83989860+sonsoeurngpel@users.noreply.github.com>
2022-06-29 07:57:00 +0700

    Update nft-detail.js

--- components/nft-detail.js
+++ components/nft-detail.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { RadioGroup } from '@headlessui/react'
-import Opensea from '../public/logos/opensea'
+import Opensea from '../public/logos/supply'
 import Link from 'next/link'
 import Image from 'next/image'
 
@@ -80,12 +80,12 @@ export default function NftDetail() {
                   </div>
                 </RadioGroup>
               </div>
-              <Link href="https://opensea.io/collection/dogevipclub" passHref>
+              <Link href="https://tofunft.com/collection/avax-story-avax/items" passHref>
               <button
                 type="submit"
                 className="mt-2 w-full bg-pale-yellow rounded-md py-3 px-8 flex items-center justify-center font-justanotherhand text-4xl text-teal focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              ><Opensea className="h-10 mr-3"/>
-                View details on Opensea
+              ><Supply className="h-10 mr-3"/>
+                View details on TofuNFT
               </button>
               </Link>
             </form>
